25d7034c9478134ddf5c74d75b198eb03d592369
Yousef N. K. ALjazzar <joseph.ana.19221@gmail.com>
2023-02-13 18:50:46 +0200

    add TextEditingController

--- lib/pages/auth/SignUpPersonalData.dart
+++ lib/pages/auth/SignUpPersonalData.dart
@@ -16,182 +16,178 @@ import 'package:provider/provider.dart';
 import '../../services/auth_provider.dart';
 
 class SignUpPersonalData extends StatelessWidget {
-   SignUpPersonalData({super.key});
-
-
-
+  SignUpPersonalData({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<AuthProvider>(
-        builder: (context, provider, x){
-          return Scaffold(
-        backgroundColor: ColorManager.backGround,
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.all(16),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              SizedBox(
-                height: 60.h,
-              ),
-              Center(
-                child: CustomTextTitle(
-                  text: 'personalDetails'.tr,
-                ),
-              ),
-                  Center(
-                    child: SizedBox(
-                      height: 100.h,
-                      width: 100.w,
-                      child: Stack(
-                        clipBehavior: Clip.none,
-                        fit: StackFit.expand,
-                        children: [
-                          CircleAvatar(
-                            foregroundImage: provider.imageFile == null
-                                ? Image.asset(
-                              ImageAssets.avatar,
-                            ).image
-                                : Image.file(
-                              provider.imageFile!,
-                              fit: BoxFit.cover,
-                            ).image,
-                            backgroundImage: provider.imageFile == null
-                                ? Image.asset(
-                              ImageAssets.avatar,
-                            ).image
-                                : Image.file(
-                              provider.imageFile!,
-                              fit: BoxFit.cover,
-                            ).image,
-                          ),
-                          Positioned(
-                              bottom: -7.h,
-                              right: -15.w,
-                              child: RawMaterialButton(
-                                onPressed: () {
-                                  showModalBottomSheet(
-                                    context: context,
-                                    builder: (builder) => provider.bottomSheet(),
-                                  );
-                                },
-                                elevation: 2.0,
-                                fillColor: Color(0xFFF5F6F9),
-                                child: Icon(Icons.camera_alt_outlined, color: Colors.blue,),
-                                padding: EdgeInsets.all(5.0),
-                                shape: CircleBorder(),
-                              )),
-                        ],
+    return Consumer<AuthProvider>(builder: (context, provider, x) {
+      return Scaffold(
+          backgroundColor: ColorManager.backGround,
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 60.h,
+                    ),
+                    Center(
+                      child: CustomTextTitle(
+                        text: 'personalDetails'.tr,
                       ),
                     ),
-                  ),
+                    Center(
+                      child: SizedBox(
+                        height: 100.h,
+                        width: 100.w,
+                        child: Stack(
+                          clipBehavior: Clip.none,
+                          fit: StackFit.expand,
+                          children: [
+                            CircleAvatar(
+                              foregroundImage: provider.imageFile == null
+                                  ? Image.asset(
+                                      ImageAssets.avatar,
+                                    ).image
+                                  : Image.file(
+                                      provider.imageFile!,
+                                      fit: BoxFit.cover,
+                                    ).image,
+                              backgroundImage: provider.imageFile == null
+                                  ? Image.asset(
+                                      ImageAssets.avatar,
+                                    ).image
+                                  : Image.file(
+                                      provider.imageFile!,
+                                      fit: BoxFit.cover,
+                                    ).image,
+                            ),
+                            Positioned(
+                                bottom: -7.h,
+                                right: -15.w,
+                                child: RawMaterialButton(
+                                  onPressed: () {
+                                    showModalBottomSheet(
+                                      context: context,
+                                      builder: (builder) =>
+                                          provider.bottomSheet(),
+                                    );
+                                  },
+                                  elevation: 2.0,
+                                  fillColor: Color(0xFFF5F6F9),
+                                  child: Icon(
+                                    Icons.camera_alt_outlined,
+                                    color: Colors.blue,
+                                  ),
+                                  padding: EdgeInsets.all(5.0),
+                                  shape: CircleBorder(),
+                                )),
+                          ],
+                        ),
+                      ),
+                    ),
 
-              const SizedBox(
-                height: 30,
-              ),
-               Text(
-                'email'.tr,
-                style: TextStyle(fontSize: 12),
-              ),
-              // const SizedBox(
-              //   height: 5,
-              // ),
-              CustomTextFormAuth(
-                hidepassword: false,
-                textInputType: TextInputType.emailAddress,
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      'email'.tr,
+                      style: TextStyle(fontSize: 12),
+                    ),
 
-                hintText: 'email'.tr,
-                //  labelText: 'البريد الالكتروني / رقم الهاتف',
-                //  iconData: Icons.email_outlined,
-              ),
-              //  const SizedBox(
-              //     height: 10,
-              //   ),
-               Text(
-                'password'.tr,
-                style: TextStyle(fontSize: 12),
-              ),
+                    CustomTextFormAuth(
+                      hidepassword: false,
+                      textInputType: TextInputType.emailAddress,
+                      myController: provider.emailTextInput,
+                      hintText: 'email'.tr,
+                    ),
 
-              CustomTextFormAuth(
-                textInputType: TextInputType.visiblePassword,
-                hidepassword: provider.hidePass,
-                pressSuffixIcon: () {
-                 provider.changeHidePass();
-                },
-                hintText: 'password'.tr,
-                // labelText: 'كلمة المرور',
-                iconData: provider.hidePass ? Icons.visibility : Icons.visibility_off,
-              ),
-               Text(
-                'userName'.tr,
-                style: TextStyle(fontSize: 12),
-              ),
-              // const SizedBox(
-              //   height: 5,
-              // ),
-              CustomTextFormAuth(
-                hidepassword: false,
-                textInputType: TextInputType.emailAddress,
+                    Text(
+                      'password'.tr,
+                      style: TextStyle(fontSize: 12),
+                    ),
 
-                hintText: 'userName'.tr,
+                    CustomTextFormAuth(
+                      textInputType: TextInputType.visiblePassword,
+                      hidepassword: provider.hidePass,
+                      pressSuffixIcon: () {
+                        provider.changeHidePass();
+                      },
+                      hintText: 'password'.tr,
+                      myController: provider.passworTextInput,
+                      // labelText: 'كلمة المرور',
+                      iconData: provider.hidePass
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                    ),
+                    Text(
+                      'userName'.tr,
+                      style: TextStyle(fontSize: 12),
+                    ),
+                    // const SizedBox(
+                    //   height: 5,
+                    // ),
+                    CustomTextFormAuth(
+                      hidepassword: false,
+                      textInputType: TextInputType.emailAddress,
+                      myController: provider.usernameTextInput,
+                      hintText: 'userName'.tr,
 
-                //  labelText: 'البريد الالكتروني / رقم الهاتف',
-                //  iconData: Icons.email_outlined,
-              ),
-               Text(
-                'DOB'.tr,
-                style: TextStyle(fontSize: 12),
-              ),
-              // const SizedBox(
-              //   height: 5,
-              // ),
-              CustomTextFormAuth(
-                myController: provider.dateTextInput,
-                pressSuffixIcon: () async {
-                  DateTime? pickedDate = await showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime(1950),
-                      lastDate: DateTime(2100));
-                  if (pickedDate != null) {
-                    print(pickedDate);
-                    String formattedDate =
-                        DateFormat('yyyy-MM-dd').format(pickedDate);
-                    print(formattedDate);
-                    provider.showDateText(formattedDate);
-                  } else {}
-                },
-                hintText: 'YYYY/MM/DD',
-                iconData: Icons.calendar_month_outlined,
-              ),
-               Text(
-                'mobileNumber'.tr,
-                style: TextStyle(fontSize: 12),
-              ),
-              // const SizedBox(
-              //   height: 5,
-              // ),
-              CustomTextFormAuth(
-                hidepassword: false,
-                textInputType: TextInputType.number,
-                iconData: Icons.phone,
-                hintText: 'mobileNumber'.tr,
-                //  labelText: 'البريد الالكتروني / رقم الهاتف',
-                //  iconData: Icons.email_outlined,
-              ),
-              CustomButtonPrimary(
-                text: 'continue'.tr,
-                onpressed: () {
-                  Get.toNamed(RouteHelper.termsAndConditions);
-                },
-              ),
-            ]),
-          ),
-        ));});
+                      //  labelText: 'البريد الالكتروني / رقم الهاتف',
+                      //  iconData: Icons.email_outlined,
+                    ),
+                    Text(
+                      'DOB'.tr,
+                      style: TextStyle(fontSize: 12),
+                    ),
+                    // const SizedBox(
+                    //   height: 5,
+                    // ),
+                    CustomTextFormAuth(
+                      myController: provider.dateTextInput,
+                      pressSuffixIcon: () async {
+                        DateTime? pickedDate = await showDatePicker(
+                            context: context,
+                            initialDate: DateTime.now(),
+                            firstDate: DateTime(1950),
+                            lastDate: DateTime(2100));
+                        if (pickedDate != null) {
+                          print(pickedDate);
+                          String formattedDate =
+                              DateFormat('yyyy-MM-dd').format(pickedDate);
+                          print(formattedDate);
+                          provider.showDateText(formattedDate);
+                        } else {}
+                      },
+                      hintText: 'YYYY/MM/DD',
+                      iconData: Icons.calendar_month_outlined,
+                    ),
+                    Text(
+                      'mobileNumber'.tr,
+                      style: TextStyle(fontSize: 12),
+                    ),
+                    // const SizedBox(
+                    //   height: 5,
+                    // ),
+                    CustomTextFormAuth(
+                      myController: provider.mobileTextInput,
+                      hidepassword: false,
+                      textInputType: TextInputType.number,
+                      iconData: Icons.phone,
+                      hintText: 'mobileNumber'.tr,
+                      //  labelText: 'البريد الالكتروني / رقم الهاتف',
+                      //  iconData: Icons.email_outlined,
+                    ),
+                    CustomButtonPrimary(
+                      text: 'continue'.tr,
+                      onpressed: () {
+                        Get.toNamed(RouteHelper.termsAndConditions);
+                      },
+                    ),
+                  ]),
+            ),
+          ));
+    });
   }
-
-
-
-
 }
--- lib/services/auth_provider.dart
+++ lib/services/auth_provider.dart
@@ -1,4 +1,3 @@
-
 import 'dart:io';
 
 import 'package:dio/dio.dart';
@@ -24,6 +23,12 @@ class AuthProvider extends ChangeNotifier {
     '01:00 - 02:00',
     '03:00 - 04:00',
   ];
+  TextEditingController dateTextInput = TextEditingController();
+  TextEditingController nameTextInput = TextEditingController();
+  TextEditingController emailTextInput = TextEditingController();
+  TextEditingController mobileTextInput = TextEditingController();
+  TextEditingController usernameTextInput = TextEditingController();
+  TextEditingController passworTextInput = TextEditingController();
   bool isCoachSelection = false;
   String dropdownValue = "choseTime".tr;
   bool isDiseases = true;
@@ -36,34 +41,26 @@ class AuthProvider extends ChangeNotifier {
   var price2 = 179.99;
   var price3 = 249.99;
 
-
-
-  selectOne(){
+  selectOne() {
     isSelectedOne = true;
     isSelectedTwo = false;
     isSelectedThree = false;
     notifyListeners();
-
   }
-  selectTow(){
+
+  selectTow() {
     isSelectedOne = false;
     isSelectedTwo = true;
     isSelectedThree = false;
     notifyListeners();
-
   }
-  selectThree(){
+
+  selectThree() {
     isSelectedOne = false;
     isSelectedTwo = false;
     isSelectedThree = true;
     notifyListeners();
-
   }
-
-
-
-
-
 
   final List<String> items = [
     '04:00 - 05:00 ',
@@ -71,80 +68,73 @@ class AuthProvider extends ChangeNotifier {
     '03:00 - 04:00 ',
   ];
   String? selectedValue;
-  makeCulturalTrue(){
-     isCultural = true;
+  makeCulturalTrue() {
+    isCultural = true;
     notifyListeners();
-
   }
-  makeCulturalFalse(){
-     isCultural = false;
+
+  makeCulturalFalse() {
+    isCultural = false;
     notifyListeners();
-
   }
-  makeIsDiseasesTrue(){
+
+  makeIsDiseasesTrue() {
     isDiseases = true;
     notifyListeners();
-
   }
-  makeIsDiseasesFalse(){
+
+  makeIsDiseasesFalse() {
     isDiseases = false;
     notifyListeners();
-
   }
-  makeCoachSelectionTrue(){
-    isCoachSelection  = true;
+
+  makeCoachSelectionTrue() {
+    isCoachSelection = true;
     notifyListeners();
-
   }
-  makeCoachSelectionFalse(){
+
+  makeCoachSelectionFalse() {
     isCoachSelection = false;
     notifyListeners();
-
   }
 
-  TextEditingController dateTextInput = TextEditingController();
-
-  showDateText(String date){
+  showDateText(String date) {
     dateTextInput.text = date;
     notifyListeners();
   }
 
-  showDropdownValue(String? date){
+  showDropdownValue(String? date) {
     dropdownValue = date!;
     notifyListeners();
   }
 
-
-  changeHidePass(){
-    hidePass=!hidePass;
+  changeHidePass() {
+    hidePass = !hidePass;
     notifyListeners();
   }
 
-  changeRememberMe(){
-    rememberMe=!rememberMe;
+  changeRememberMe() {
+    rememberMe = !rememberMe;
     notifyListeners();
   }
 
-  changeIsChecked(bool? value){
-    isChecked=value!;
+  changeIsChecked(bool? value) {
+    isChecked = value!;
     notifyListeners();
   }
-  login(String userName,String password) async {
-    try{
+
+  login(String userName, String password) async {
+    try {
       bool? respontLogin = await DioClient.dioClient.login(userName, password);
-    }on DioError catch(e){
-      String massage=DioException.fromDioError(e).toString();
+    } on DioError catch (e) {
+      String massage = DioException.fromDioError(e).toString();
       final snackBar = SnackBar(
-        content: SizedBox(
-            height: 32.h,
-            child:   Center(
-                child: Text(massage ))),
+        content: SizedBox(height: 32.h, child: Center(child: Text(massage))),
         backgroundColor: ColorManager.red,
         behavior: SnackBarBehavior.floating,
         width: 300.w,
         duration: const Duration(seconds: 1),
       );
-
     }
     notifyListeners();
   }
@@ -179,18 +169,20 @@ class AuthProvider extends ChangeNotifier {
     }
   }
 
-  openCamera(){
+  openCamera() {
     _getFromCamera();
     Navigator.pop(context);
   }
-  openGallery(){
+
+  openGallery() {
     _getFromGallery();
     Navigator.pop(context);
   }
+
   Widget bottomSheet() {
     return Container(
       decoration:
-      BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(30))),
+          BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(30))),
       height: 220.h,
       width: MediaQuery.of(context).size.width,
       margin: EdgeInsets.all(20),
@@ -205,9 +197,11 @@ class AuthProvider extends ChangeNotifier {
           CustomButtonPrimary(
               text: "openCamera".tr,
               onpressed: () {
-               openCamera();
+                openCamera();
               }),
-          SizedBox(height: 10.h,),
+          SizedBox(
+            height: 10.h,
+          ),
           Text("or".tr),
           CustomButtonPrimary(
               text: "openGallery".tr,
