879fc94385cbadfb846b686c9c2bdca9c8bd8961
Ahmad Abu Ghali <38799135+AhmadMAbuGhali@users.noreply.github.com>
2023-02-20 18:41:45 +0200

    provider  and dio

--- lib/models/login_model.dart
+++ lib/models/login_model.dart
@@ -1,21 +1,24 @@
 class LoginModel {
   String? token;
   DateTime? expiration;
+  String? role;
 
-  LoginModel({this.token, this.expiration});
+  LoginModel({this.token, this.expiration,this.role});
 
   LoginModel.fromJson(Map<String, dynamic> json) {
     token = json['token'];
     if(json['expiration'] !=null){
       expiration=DateTime.parse(json['expiration']);
     }
+    role=json["role"];
 
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['token'] = this.token;
-    data['expiration'] = this.expiration;
+    data['token'] = token;
+    data['expiration'] = expiration;
+    data["role"]= role;
     return data;
   }
 }
